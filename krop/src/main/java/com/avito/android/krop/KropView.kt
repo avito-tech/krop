ec0a376db394d8089ef960e09f714d35ca935700
Aleksandr Kutashov <aakutashov@avito.ru>
2021-02-24 22:58:28 +0300

    SEL-15074: Adds animated scale style for image rotation method
    Old (instant) scale animation is suitable for small angles rotation, but it accumulates error while used multiple times (i.e. wide angle rotation is presented as a number of small rotations).
    For such cases, there is animated scale, which normally should be conducted when rotation is finished and space fix is required.
    Rotation could be used without any fixes though.

--- krop/src/main/java/com/avito/android/krop/KropView.kt
+++ krop/src/main/java/com/avito/android/krop/KropView.kt
@@ -12,6 +12,7 @@ import android.os.Parcelable
 import android.util.AttributeSet
 import android.widget.FrameLayout
 import androidx.annotation.WorkerThread
+import com.avito.android.krop.util.ScaleAfterRotationStyle
 import com.avito.android.krop.util.Transformation
 
 class KropView(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
@@ -65,7 +66,8 @@ class KropView(context: Context, attrs: AttributeSet) : FrameLayout(context, att
         addView(overlayView)
     }
 
-    fun rotateBy(angle: Float) = imageView.rotateBy(angle)
+    fun rotateBy(angle: Float, scaleAnimation: ScaleAfterRotationStyle = ScaleAfterRotationStyle.NONE) =
+            imageView.rotateBy(angle, scaleAnimation)
 
     fun setZoom(scale: Float) {
         imageView.setZoom(scale)
